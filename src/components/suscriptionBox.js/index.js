a727ef920074d49794b73564050623f9b739ced6
balvinder-chapter247 <balvinder.s@chapter247.com>
2022-02-02 19:54:16 +0530

    some changes

--- src/components/suscriptionBox.js/index.js
+++ src/components/suscriptionBox.js/index.js
@@ -1,16 +1,20 @@
+import { useState } from "react"
+import validate from 'validate.js';
+
 const SuscriptionBox = () => {
+   
     return (
         <>
-            <form action="">
+            <form >
                 <div className="text-black">
                     <div className="container  flex justify-center items-center">
                         <div className="relative">
-                            <input type="text"
+                            <input type="email"
                                 className="h-10 w-96 pl-5 pr-5  focus:shadow focus:outline-none"
                                 placeholder="Subscribe to our Newsletter" />
                             <div className="absolute top-1 right-1">
-                                <button className="h-8 w-20 text-white  bg-blue  
-                                ">
+                                <button className="h-8 w-20 text-white bg-blue"
+                                type="submit">
                                     Subscribe
                                 </button>
                             </div>
