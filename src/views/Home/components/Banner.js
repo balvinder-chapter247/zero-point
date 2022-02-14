ae0bfce67668a83ff765ad2267d5b2b8e5ccc46d
balvinder-chapter247 <balvinder.s@chapter247.com>
2022-02-14 11:49:47 +0530

    some changes

--- src/views/Home/components/Banner.js
+++ src/views/Home/components/Banner.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import bannerImage from '../../../assets/Images/Home/bannerimg.gif'
 const BannerSection = () =>
 {
     return(
         <>
-        
+        <section>
+            <div>
+                <img src={bannerImage} alt="bannerImage" className='homeBannerImage'/>
+            </div>
+            
+        </section>
         </>
     )
 }
--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import BannerSection from './components/Banner';
+import TopCourses from './components/TopCourses';
 
 const Home = () => {
-  
-  
+
+
     return (
         <>
-           <BannerSection/>
+            <BannerSection />
+
+            <div className=" mx-auto  container px-4">
+                <div className="grid grid-cols-3 gap-2 my-12 justify-items-center">
+
+                    <TopCourses />
+                    <TopCourses />
+                    <TopCourses />
+                    <TopCourses />
+                    <TopCourses />
+                    <TopCourses />
+
+                </div>
+            </div>
+
+
         </>
     )
 
